ea6c8ba86568c543bd92b279ce28ef42ea2367de
jesus.gil <jesus.gil@thoughtworks.com>
2025-07-08 10:51:34 +0200

    Fixing git

--- obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5904b81afe9dfa3450ce9ecc3374dab8ef08d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee3a1e0e897ab7cfabe39589c1736763208e00ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
